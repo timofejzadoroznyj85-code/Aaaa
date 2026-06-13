432a4cbb6dcc7791fdd5ad913ab09617df65a554
timofejzadoroznyj85-code <timofejzadoroznyj85@gmail.com>
2026-06-13 17:10:47 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "5284453931"))
+BOT_USERNAME = os.getenv("BOT_USERNAME", "Soulstarsssbot")
+
+PIARFLOW_API_KEY = os.getenv("PIARFLOW_API_KEY”)
+PIARFLOW_BASE_URL = "https://piarflow.com/v1"
+
+TASK_REWARD = 0.3
+REFERRAL_REWARD = 2.0
+
